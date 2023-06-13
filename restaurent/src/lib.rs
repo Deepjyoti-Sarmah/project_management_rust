a7af99f3a9b05ae81592765a132ac42336d521f7
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-13 12:35:06 +0530

    module privacy using struct and enum

--- restaurent/src/lib.rs
+++ restaurent/src/lib.rs
@@ -12,18 +12,84 @@
 //         assert_eq!(result, 4);
 //     }
 // }
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+//module privacy
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+
+//         // fn seat_at_table() {}
+//     }
+
+//     // mod serving{
+//     //     fn take_order() {}
+
+//     //     fn serve_order() {}
+
+//     //     fn take_payment() {}
+//     // }
+
+//     pub fn eat_at_restaurant() {
+//         //Absolute path
+//         crate::front_of_house::hosting::add_to_waitlist();
+
+//         //Relative path
+//         front_of_house::hosting::add_to_waitlist();
+//     }
+// }
+
+// fn serve_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::serve_order(); //super allows us to reference parent module
+//     }
+
+//     fn cook_order() {}
+// }
+
+
+//privacy module using struct
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast{
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     let mut meal: Breakfast = back_of_house::Breakfast::summer("Rye");
+
+//     //does not work
+//     // let meal2: Breakfast = back_of_house::Breakfast{
+//     //     toast: String::from("Wheat"),
+//     //     seasonal_fruit: String::from("peaches")
+//     // };
+
+//     meal.toast = String::from("Wheat"); // toast field is private
+// }
+
+mod back_of_house{
+    pub enum Appetizer {
+        Soup, 
+        Salad,
     }
+}
 
-    mod serving{
-        fn take_order() {}
+pub fn eat_at_restaurant() {
+    let order1: Appetizer = back_of_house::Appetizer::Soup;
+    let order2: Appetizer = back_of_house::Appetizer::Salad;
 
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
 }
